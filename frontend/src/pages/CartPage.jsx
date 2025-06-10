44a4952b85560f52a09d1111434e26f1488801e3
Mohammad Iqram <89983132+MohammadIqram@users.noreply.github.com>
2025-06-10 09:49:51 +0530

    Update CartPage.jsx

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -20,19 +20,19 @@ const CartPage = () => {
 						animate={{ opacity: 1, x: 0 }}
 						transition={{ duration: 0.5, delay: 0.2 }}
 					>
-						{cart.length === 0 ? (
+						{cart?.length === 0 ? (
 							<EmptyCartUI />
 						) : (
 							<div className='space-y-6'>
-								{cart.map((item) => (
+								{cart?.map((item) => (
 									<CartItem key={item._id} item={item} />
 								))}
 							</div>
 						)}
-						{cart.length > 0 && <PeopleAlsoBought />}
+						{cart?.length > 0 && <PeopleAlsoBought />}
 					</motion.div>
 
-					{cart.length > 0 && (
+					{cart?.length > 0 && (
 						<motion.div
 							className='mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full'
 							initial={{ opacity: 0, x: 20 }}
