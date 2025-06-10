7f9667be5f701eb04ac5b511a467c0dda0b9facf
Mohammad Iqram <89983132+MohammadIqram@users.noreply.github.com>
2025-06-10 11:06:42 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: import.meta.VITE_MODE === "development" ? "http://localhost:5000/api" : import.meta.VITE_BACKEND_URL,
+	baseURL: "https://api.sharwings.in/api",
 	withCredentials: true, // send cookies to the server
 });
 
