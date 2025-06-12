7ec2ea150624d230be0d591abc98275ca6e79058
Mohammad Iqram <89983132+MohammadIqram@users.noreply.github.com>
2025-06-12 08:22:31 +0530

    Update payment.route.js

--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { checkoutSuccess, createCheckoutSession, createCheckoutSessionRazorpay } from "../controllers/payment.controller.js";
+import { checkoutSuccess, createCheckoutSession, createCheckoutSessionRazorpay, razorpaySuccess } from "../controllers/payment.controller.js";
 
 const router = express.Router();
 
 router.post("/create-checkout-session", protectRoute, createCheckoutSession);
 router.post("/checkout-success", protectRoute, checkoutSuccess);
 router.post("/create-checkout-session-razorpay", protectRoute, createCheckoutSessionRazorpay);
+router.post("/razorpay-success", protectRoute, razorpaySuccess);
 
 export default router;
