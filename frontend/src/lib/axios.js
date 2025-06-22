04b61345651ea7505c60a454b199fe43b3b8f28d
MohammadIqram <mohammadiqram001@gmail.com>
2025-06-22 22:44:01 +0530

    UPDATE: axios url

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-	baseURL: "https://sharwings.in/api",
+	baseURL: "https://www.sharwings.in/api",
 	withCredentials: true, // send cookies to the server
 });
 
