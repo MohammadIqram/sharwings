e680e0de08a67f291d2f4c7e963523b96abe2d5f
MohammadIqram <mohammadiqram001@gmail.com>
2025-06-22 12:46:25 +0530

    UPDATE: discount banner on bottom

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -23,7 +23,6 @@ const HomePage = () => {
 	return (
 		<div className='relative min-h-screen text-white overflow-hidden'>
 			<div className='relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-16'>
-				<HomePageBanner />
 				<h1 className='text-center text-5xl sm:text-6xl font-bold text-emerald-400 mb-4'>
 					Explore Our Categories
 				</h1>
@@ -38,6 +37,9 @@ const HomePage = () => {
 				</div>
 
 				{!isLoading && products?.length > 0 && <FeaturedProducts featuredProducts={products} />}
+			</div>
+			<div className="max-w-7xl mx-auto">
+				<HomePageBanner />
 			</div>
 		</div>
 	);
